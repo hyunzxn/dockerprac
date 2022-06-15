b29b43358cba0c58a4744d69bfd026c7466fb872
hyunzxn <jann1653@gmail.com>
2022-06-15 10:40:54 +0900

    Initial commit

---
+++ src/main/java/com/example/dockerprac/DockerpracApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerpracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerpracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerprac/DockerpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerpracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
